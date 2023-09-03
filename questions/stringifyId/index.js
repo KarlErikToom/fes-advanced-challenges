277337ae328d3f1281e72c83a263f31b46e2a715
Karl <toom.karl42@gmail.com>
2023-09-03 18:48:09 +0300

    awnsered question 4 and 5

--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -17,7 +17,11 @@
  */
 
 const stringifyId = (arr) => {
+  const idArray = arr.map((obj) => obj.id);
+  const uniqueArray = [...new Set(idArray)];
 
+  
+  return uniqueArray.join(", ");
 };
 
 module.exports = stringifyId;
